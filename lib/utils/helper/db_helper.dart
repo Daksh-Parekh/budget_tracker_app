8f590e260012eabf63632ce2bf786a40755781eb
Daksh-Parekh <dakshparekh42@gmail.com>
2025-01-02 14:33:23 +0530

    creating a database& table

--- lib/utils/helper/db_helper.dart
+++ lib/utils/helper/db_helper.dart
@@ -1,1 +1,22 @@
+import 'package:sqflite/sqflite.dart';
 
+class DBHelper {
+  DBHelper._();
+  static DBHelper dbHelper = DBHelper._();
+  //Creating a Database
+  Future<void> initDB() async {
+    String dbPath = await getDatabasesPath();
+    String path = '${dbPath}budget.db';
+    //Open a DataBase
+    Database db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, _) async {
+        //Creating a table
+        String query =
+            'CREATE TABLE category(cat_id INTEGER PRIMARY KEY AUTOINCREMENT,cat_name TEXT NOT NULL,cat_image BLOB NOT NULL)';
+        await db.execute(query);
+      },
+    );
+  }
+}
